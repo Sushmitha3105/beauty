7742eef610b0e0fa0471963800cde2ad10b3ab90
Sushmitha <keesarisushmitha@gmail.com>
2024-05-03 15:45:30 +0530

    React

--- App.jsx
+++ App.jsx
@@ -3,30 +3,36 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-
-const person = {
-  name: 'Gregorio Y. Zara',
-  theme: {
-    backgroundColor: 'Pink',
-    color: 'black'
-  
-  }
-};
-
-export default function TodoList() {
+function Item({ name, isPacked }) {
   return (
-    <div style={person.theme}>
-      <h1>{person.name}'s Todos</h1>
-      <img
-        className="avatar"
-        src="https://i.imgur.com/7vQD0fPs.jpg"
-        alt="Gregorio Y. Zara"
-      />
+    <li className="item">
+      
+      {name} {isPacked && '✔'}
+    </li>
+  );
+}
+
+export default function PackingList() {
+  
+  return (
+    
+    <section>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis ipsum, doloribus praesentium numquam asperiores explicabo ad obcaecati. Quo, ut ipsum.</p>
+      <h2>Sally Ride's Packing List</h2>
       <ul>
-        <li>Improve the videophone</li>
-        <li>Prepare aeronautics lectures</li>
-        <li>Work on the alcohol-fuelled engine</li>
+        <Item
+          isPacked={true}
+          name="Space suit"
+        />
+        <Item
+          isPacked={true}
+          name="Helmet with a golden leaf"
+        />
+        <Item
+          isPacked={false}
+          name="Photo of Tam"
+        />
       </ul>
-    </div>
+    </section>
   );
 }
